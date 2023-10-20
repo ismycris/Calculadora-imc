d57634169d4ff43371681b4eb844cf480b19e54e
ismycris <cristielythainara@gmail.com>
2023-10-20 14:40:31 -0300

    atualização

--- assents/js/script.js
+++ assents/js/script.js
@@ -1,20 +1,47 @@
 const form = document.getElementById('form');
-
-form.addEventListener('submit', function (event) {
+form.addEventListener('submit', function(event) {
     event.preventDefault();
 
-    const weight = document.getElementById('weight').value;
-    const height = document.getElementById('height').value;
+    const weight = parseFloat(document.getElementById('weight').value);
+    const height = parseFloat(document.getElementById('height').value);
+
     const bmi = (weight / (height * height)).toFixed(2);
 
-    const value = this.getElementsByClassName('value');
-    let description = '';
+    if (!isNaN(bmi)) {
+        const value = document.getElementById('value');
+        let description = '';
 
-    document.getElementById('infos').classList.add('hidden');
+        value.classList.add('attention');
+        
+        document.getElementById('infos').classList.remove('hidden');
+    
+        if (bmi < 18.5) {
+            description = 'Cuidado! Você está abaixo do peso!';
+        } 
 
-    if(bmi < 18.5){
-        description = 'cuidado! vocë está abaixo do peso!'
+        else if (bmi >= 18.5 && bmi <= 25) {
+            description = "Você está no peso ideal!";
+            value.classList.remove('attention');
+            value.classList.add('normal');
+        }
 
+        else if (bmi > 25 && bmi <= 30) {
+            description = "Cuidado! Você está com sobrepeso!";
+        }
+
+        else if (bmi > 30 && bmi <= 35) {
+            description = "Cuidado! Você está com obesidade moderada!";
+        }
+
+        else if (bmi > 35 && bmi <= 40) {
+            description = "Cuidado! Você está com obesidade severa!";
+        }
+
+        else {
+            description = "Cuidado! Você está com obesidade mórbida!";
+        }
+
+        value.textContent = bmi.replace('.', ',');
+        document.getElementById('description').textContent = description;
     }
-    document.getElementById('description').textContent = description;
 });
